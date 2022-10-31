7e7b76e88cee0509039cd195a314885d5d300a74
tomo960714 <90306200+tomo960714@users.noreply.github.com>
2022-10-31 04:36:54 +0100

    Layout updates

--- UI/ui.py
+++ UI/ui.py
@@ -29,23 +29,46 @@ sg.theme('SystemDefaultForReal')
 img= Image.open('Data\image.png')
 #resize image
 
-new_height = 300
+new_height = 100
 new_width = int(np.ceil(img.size[0]*new_height/img.size[1]))
 
 print(img.size)
 print(new_width,new_height)
 img = img.resize((new_width,new_height))
 print(img.size)
-""" 
+
+radar_col = [
+    [
+        sg.Canvas(key='-RADAR_CANVAS-')
+        ]
+]
+player_left_col = [
+    [sg.Image(source='Data\image.png',size=img.size,key='-NAT_FLAG-')], #country flag
+    [sg.Text("country",key ='-NAT-')], #country
+    [sg.Text("Overal rating",key ='-OVR-')]#overal rating of the player
+]
+player_right_col = [
+    [sg.Image(source='Data\image.png',size=img.size,key='-CLUB_FLAG-')], #club flag
+    [sg.Text("club",key ='-CLUB-')], #club
+    [sg.Text("position",key ='-POS-')]#Player position
+]
+player_col =[
+    [sg.Text("Player Name",key ='-PLAYER_NAME-')], #Player name
+    [sg.Image(source='Data\image.png',size=img.size,key ='-PLAYER_FACE-')], #Player face
+    [sg.Column(player_left_col),sg.VSeperator(),sg.Column(player_right_col)]
+]
+layout = [[sg.Column(radar_col),sg.VSeperator(),sg.Column(player_col)]]
+"""
 layout = [  [sg.Text(init_player.name)],
             [sg.Image(source=init_player.Photo,size=img.size,key = "-PLAYER_IMAGE-",)],
             [sg.Button('OK'),sg.Button('Cancel')]
             ]
-"""
+
 layout = [  [sg.Text('asd')],
             [sg.Image(source='Data\image.png',size=img.size,key = "-IMAGE-",)],
             [sg.Button('OK'),sg.Button('Cancel')]
             ]
+"""
 window = sg.Window(ui_c.MAIN_TITLE,layout).read()
 
 
